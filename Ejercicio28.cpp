ed7f840fcebb3247f447610ca2391bb5a303aefe
laums25 <lm.mercado@uniandes.edu.co>
2019-11-14 03:58:28 +0000

    algo

--- Ejercicio28.cpp
+++ Ejercicio28.cpp
@@ -2,71 +2,87 @@
 #include <fstream>
 #include <cmath>
 
-float g = 9.8;
-float k = 0.9;
-float delta = 0.01;
-int N = 25;
-float ang = 34.0;
-float v0 = 22.0;
-float v0x = v0* cos(ang*M_PI/180.0);
-float v0y = v0* sin(ang*M_PI/180.0);
+using namespace std;
+
+
+const double g = 9.8;
+const double k = 0.9;
+const double delta = 0.01;
+const int N = 25;
+const double ang = 34.0;
+const double v0 = 22.0;
+const double v0x = v0* cos(ang*M_PI/180.0);
+const double v0y = v0* sin(ang*M_PI/180.0);
+
+void rk(double t, double delta, double p[]);
+double pro(double t, double p[], int i);
+
 
 int main(void)
 {
-  ofstream fout("datos-array.txt");
-  float x = 0.0, y = 0.0;
-  float time;
+  //ofstream fout("datos-array.dat");
+  double time;
   double p[N] = {1, 0};
-  for(time = 0; time <= 10; time += DeltaT) {
-    fout << time << "\t" << y[0] << "\t" << y[1] << endl;
-    rk4_vector(time, DeltaT, y);
+  for(time = 0; time <= 10; time += delta) {
+    cout << time << "\t" << p[0] << "\t" << p[1] << endl;
+    rk(time, delta, p);
   }
-  fout.close();
+  //fout.close();
 
   return 0;
 }
 
-void pro(double t, double y0, double y1){
-    float vt= sqrt(v0x*v0x + v0y*v0y);
-    float dx=- k*v0x*v0x/vt;
-    float dy=- g -k *v0y*v0y / vt;
+double pro(double t, double p[], double i)
+{
+  if (0 == i) {
+    return p[1];
+  }
+  else if (1 == i) {
+    double vt= sqrt(v0x*v0x + v0y*v0y);  
+    double dx=- k*v0x*v0x/vt; 
+    double dy=- g -k *v0y*v0y / vt;
+    return dy;
+  }
+  else {
+    std::cerr << "ERROR. Asking unexistent function f, with i = " <<  i << std::endl;
+  }
 }
 
 
-float rk(float t, float delta, float y[]){
-    float k1[N], k2[N], k3[N], k4[N];
-    float yt[N];
+void rk(double t, double delta, double p[]){
+    double k1[N], k2[N], k3[N], k4[N];
+    double yt[N];
     int i;
     for (i=0; i<N; ++i)
     {
-        k1[i] = delta * pro(t,y,i);
+        k1[i] = delta * pro(t,p,i);
     }
     for (i=0; i<N; ++i)
     {
-        yt[i] = y[i] + k1[i]/2;
+        yt[i] = p[i] + k1[i]/2;
     }
     for (i=0; i<N; ++i)
     {
-        k2[i] = delta * pro(t + h/2, y,i);
+        k2[i] = delta * pro(t + delta/2, p,i);
     }
     for (i=0; i<N; ++i)
     {
-        yt[i] = y[i] + k2[i]/2;
+        yt[i] = p[i] + k2[i]/2;
     }
     for (i=0; i<N; ++i)
     {
-        k3[i] = delta * pro(t + h/2, y,i);
+        k3[i] = delta * pro(t + delta/2, p,i);
     }
     for (i=0; i<N; ++i)
     {
-        yt[i] = y[i] + k3[i];
+        yt[i] = p[i] + k3[i];
     }  
     for (i=0; i<N; ++i)
     {
-        k4[i] = delta * pro(t + h, y,i);
+        k4[i] = delta * pro(t + delta, p,i);
     }
     for (i=0; i<N; ++i)
     {
-        y[i] = y[i] + (1.0/6.0)*(k1[i]+ 2*k2[i] + 2*k3[i] + k4[i]);
+        p[i] = p[i] + (1.0/6.0)*(k1[i]+ 2*k2[i] + 2*k3[i] + k4[i]);
     } 
 }
